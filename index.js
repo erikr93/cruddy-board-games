14ad2c44410b1cad25b9a5487740bae4a018947a
Steve Geluso <stevegeluso@gmail.com>
2016-12-20 12:30:44 -0800

    adding edit functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs');
 var ejsLayouts = require("express-ejs-layouts");
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(ejsLayouts);
@@ -9,14 +10,41 @@ app.use(ejsLayouts);
 // this sets a static directory for the views
 app.use(express.static(path.join(__dirname, 'static')));
 
+// tell your app to use the module
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.render('home', {games: getGames()});
 });
 
+app.get('/boardgames/new', function(req, res) {
+  res.render('boardgame-create');
+});
+
+app.post('/boardgames', function(req, res) {
+  var games = getGames();
+  games.push(req.body);
+  saveGames(games);
+
+  var path = '/boardgames/' + (games.length - 1);
+  res.redirect(path);
+});
+
+app.get('/boardgames/:id/edit', function(req, res) {
+  var game = getGames()[req.params.id];
+  res.render('boardgame-edit', {game: game});
+});
+
+app.put('/boardgames/:id', function(req, res) {
+  console.log("body:", req.body);
+  res.send(req.body);
+});
+
 app.get('/boardgames/:id', function(req, res) {
   var game = getGames()[req.params.id];
+  game.id = req.params.id;
   res.render('boardgame-detail', {game: game});
 });
 
