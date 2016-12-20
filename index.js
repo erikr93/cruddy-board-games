247897d5b6af6da6e7137bfd7c232fc4194f8daf
Steve Geluso <stevegeluso@gmail.com>
2016-12-20 10:54:58 -0800

    moving global GAMES variable to new getGames() function

--- index.js
+++ index.js
@@ -10,29 +10,33 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 app.set('view engine', 'ejs');
 
-var GAMES = [
-  {
-    name: "Settlers of Catan",
-    description: "Build, trade, score points."
-  },
-  {
-    name: "Risk",
-    description: "Battle, roll dice, kill."
-  },
-  {
-    name: "Stratego",
-    description: "Hidden battle configuration tap tap who are you dead game. Two players."
-  }
-];
-
 app.get('/', function(req, res) {
-  res.render('home', {games: GAMES});
+  res.render('home', {games: getGames()});
 });
 
 app.get('/boardgames/:id', function(req, res) {
-  var game = GAMES[req.params.id];
+  var game = getGames()[req.params.id];
   res.render('boardgame-detail', {game: game});
 });
+
+// Returns a list of all games.
+function getGames() {
+  var games = [
+    {
+      name: "Settlers of Catan",
+      description: "Build, trade, score points."
+    },
+    {
+      name: "Risk",
+      description: "Battle, roll dice, kill."
+    },
+    {
+      name: "Stratego",
+      description: "Hidden battle configuration tap tap who are you dead game. Two players."
+    }
+  ];
+  return games;
+}
 
 var port = 3000;
 console.log("http://localhost:" + port);
